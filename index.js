54075f750aaf955b2811257001bd911182663c29
¨Edgar <s.steinberg2019@gmail.com>
2024-06-23 23:39:26 -0300

    fecha two

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function submitProde() {
     });
 }
  */
-
+/** 
 const partidos = [
     { fecha: '21:00 - 20/6', grupo: 'Grupo A', equipo1: { nombre: 'Argentina', bandera: 'https://flagcdn.com/ar.svg' }, equipo2: { nombre: 'Canadá', bandera: 'https://flagcdn.com/ca.svg' } },
     { fecha: '21:00 - 21/6', grupo: 'Grupo A', equipo1: { nombre: 'Chile', bandera: 'https://flagcdn.com/cl.svg' }, equipo2: { nombre: 'Perú', bandera: 'https://flagcdn.com/pe.svg' } },
@@ -42,16 +42,59 @@ const partidos = [
     { fecha: '19:00 - 24/6', grupo: 'Grupo D', equipo1: { nombre: 'Colombia', bandera: 'https://flagcdn.com/co.svg' }, equipo2: { nombre: 'Paraguay', bandera: 'https://flagcdn.com/py.svg' } },
     { fecha: '22:00 - 24/6', grupo: 'Grupo D', equipo1: { nombre: 'Brasil', bandera: 'https://flagcdn.com/br.svg' }, equipo2: { nombre: 'Costa Rica', bandera: 'https://flagcdn.com/cr.svg' } }
 ];
+**/
+const partidos = [
+    { fecha: '19:00 - 25/6', grupo: 'Grupo A', equipo1: { nombre: 'Perú', bandera: 'https://flagcdn.com/pe.svg' }, equipo2: { nombre: 'Canadá', bandera: 'https://flagcdn.com/ca.svg' } },
+    { fecha: '22:00 - 25/6', grupo: 'Grupo A', equipo1: { nombre: 'Chile', bandera: 'https://flagcdn.com/cl.svg' }, equipo2: { nombre: 'Argentina', bandera: 'https://flagcdn.com/ar.svg' } },
+    { fecha: '19:00 - 26/6', grupo: 'Grupo B', equipo1: { nombre: 'Ecuador', bandera: 'https://flagcdn.com/ec.svg' }, equipo2: { nombre: 'Jamaica', bandera: 'https://flagcdn.com/jm.svg' } },
+    { fecha: '22:00 - 26/6', grupo: 'Grupo B', equipo1: { nombre: 'Venezuela', bandera: 'https://flagcdn.com/ve.svg' }, equipo2: { nombre: 'México', bandera: 'https://flagcdn.com/mx.svg' } },
+    { fecha: '19:00 - 27/6', grupo: 'Grupo C', equipo1: { nombre: 'Panamá', bandera: 'https://flagcdn.com/pa.svg' }, equipo2: { nombre: 'Estados Unidos', bandera: 'https://flagcdn.com/us.svg' } },
+    { fecha: '22:00 - 27/6', grupo: 'Grupo C', equipo1: { nombre: 'Uruguay', bandera: 'https://flagcdn.com/uy.svg' }, equipo2: { nombre: 'Bolivia', bandera: 'https://flagcdn.com/bo.svg' } },
+    { fecha: '19:00 - 28/6', grupo: 'Grupo D', equipo1: { nombre: 'Colombia', bandera: 'https://flagcdn.com/co.svg' }, equipo2: { nombre: 'Costa Rica', bandera: 'https://flagcdn.com/cr.svg' } },
+    { fecha: '22:00 - 28/6', grupo: 'Grupo D', equipo1: { nombre: 'Paraguay', bandera: 'https://flagcdn.com/py.svg' }, equipo2: { nombre: 'Brasil', bandera: 'https://flagcdn.com/br.svg' } }
+];
 
+const participantes = [
+    {
+        nombre: 'Tincho',
+        img: './img/aguara.jpg'
+    },
+    {
+        nombre: 'Memeito',
+        img: './img/aguara.jpg'
+    },
+    {
+        nombre: 'Marto',
+        img: './img/aguara.jpg'
+    },
+    {
+        nombre: 'Sucio Can',
+        img: './img/aguara.jpg'
+    },
+    {
+        nombre: 'K K K BEZA',
+        img: './img/aguara.jpg'
+    },
+    {
+        nombre: 'Cagavedgar',
+        img: './img/aguara.jpg'
+    },
+    {
+        nombre: 'El Negro73',
+        img: './img/aguara.jpg'
+    }
+]
 
 // Función para generar los partidos dinámicamente
 function generateMatches() {
     const container = document.getElementById('matchesContainer');
-    
+
     partidos.forEach((partido, index) => {
+        // Con createElemente creo un div y lo almceno en una variable
         const matchDiv = document.createElement('div');
+        // A mi variable le agrego una clase para css
         matchDiv.classList.add('match');
-        
+        // A mi variable le inserto un innerHTML para crear mi partido
         matchDiv.innerHTML = `
             <p>${partido.fecha} - ${partido.equipo1.nombre} vs. ${partido.equipo2.nombre} (${partido.grupo})</p>
             <div class="team">
@@ -62,7 +105,7 @@ function generateMatches() {
                 <img src="${partido.equipo2.bandera}" alt="${partido.equipo2.nombre}" class="flag">
             </div>
         `;
-        
+
         container.appendChild(matchDiv);
     });
 }
@@ -72,13 +115,30 @@ document.addEventListener('DOMContentLoaded', generateMatches);
 
 // Función para procesar y mostrar los resultados del prode
 function submitProde() {
+
     const nombre = document.getElementById('name').value;
     const resultsContainer = document.getElementById('results');
+
+    const campeon = document.getElementById('campeon').value;
+
     resultsContainer.innerHTML = ''; // Limpiar contenido anterior si hay alguno
 
     const nombreElement = document.createElement('p');
     nombreElement.textContent = `Nombre: ${nombre}`;
     resultsContainer.appendChild(nombreElement);
+
+    const participante = participantes.find(participante => participante.nombre.toLowerCase() === nombre.toLowerCase());
+    if (participante) {
+        const imgElement = document.createElement('img');
+        imgElement.src = participante.img;
+        imgElement.alt = participante.nombre;
+        imgElement.classList.add('participant-img'); // Agrega una clase CSS
+        resultsContainer.appendChild(imgElement);
+    }
+
+    const campeonElement = document.createElement('p');
+    campeonElement.textContent = `Seleccion Campeona: ${campeon}`
+    resultsContainer.appendChild(campeonElement)
 
     partidos.forEach((partido, index) => {
         const resultado1 = document.getElementById(`result_${index}_1`).value || 0;
@@ -89,3 +149,16 @@ function submitProde() {
         resultsContainer.appendChild(resultadoPartido);
     });
 }
+
+const jugadoresContainer = document.getElementById('jugadores')
+participantes.forEach(jugador => {
+    const jugadorDiv = document.createElement('div')
+    jugadorDiv.classList.add('jugadores')
+    jugadorDiv.innerHTML = `
+        <p>${jugador.nombre}</p>
+        <img src=${jugador.img} alt=${jugador.nombre} class="participant-img">
+    `
+
+    jugadoresContainer.appendChild(jugadorDiv)
+})
+//mongodb://host.docker.internal:27017/coder_53120
